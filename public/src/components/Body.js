2935a8ab20b0232ac6c981592b4eb18d460a726e
Nisarg Patel <npate236@uic.edu>
2018-09-05 23:28:23 -0500

    bug fix change body from string to innerhtml

--- public/src/components/Body.js
+++ public/src/components/Body.js
@@ -77,8 +77,7 @@ class Product extends React.Component {
                 <div className="col-md-3">
                     <img src={this.props.data.img} className="display" />
                 </div>
-                <div className="col-md-9">
-                    {this.props.data.body}
+                <div className="col-md-9" dangerouslySetInnerHTML={{__html: this.props.data.body}}>
                 </div>
             </div>
         </li>;
