6390c111ba5e948252e2daade9e8756d37ae46cb
Nisarg Patel <npate236@uic.edu>
2018-09-09 14:24:36 -0500

    install @babel/polyfill and add it to the entry way

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-    entry: path.resolve(__dirname, 'public/src/app.js'),
+    entry: ["@babel/polyfill", path.resolve(__dirname, 'public/src/app.js')],
     output: {
         path: path.resolve(__dirname, 'public/build'),
         filename: 'bundle.js'
